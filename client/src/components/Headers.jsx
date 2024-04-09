84955c1f74a04107458d7a0b36ee918d1a3ffc8e
rajesh kumar <rajeshvk2003@gmail.com>
2024-04-09 22:08:16 +0530

    server setup

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -7,7 +7,7 @@ function Headers() {
   const path=useLocation().pathname;
   return (
     <>   
-        <Navbar className="">
+        <Navbar className="bg-slate-800">
          <Link to="/" className="self-center text-nowrap text-sm md:text-lg dark:text-white">
          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500
           to-pink-500 rounded-lg text-white
