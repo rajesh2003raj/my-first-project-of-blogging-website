c95047204873d671e4494674597fa465f730bafb
rajesh kumar <rajeshvk2003@gmail.com>
2024-04-14 19:50:48 +0530

    dark mode made

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -1,17 +1,19 @@
 import { Link, useLocation} from "react-router-dom"
 import { Navbar, Button,TextInput, Dropdown, Avatar}  from  "flowbite-react"
-import { FaSearch  } from "react-icons/fa";
-import { MdDarkMode } from "react-icons/md";
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { AiOutlineSearch } from 'react-icons/ai';
 import {  HiLogout } from "react-icons/hi";
-import {useSelector} from 'react-redux'
+import {useSelector} from 'react-redux';
+ import {toggleTheme} from '../redux/store/theme/themeSlice'
+ import { useDispatch } from "react-redux";
 function Headers() {
-
+    const dispatch=useDispatch();
     const {currentUser}=useSelector((state)=>state.user);
-
+     const {theme}=useSelector((state)=>state.theme);
   const path=useLocation().pathname;
   return (
     <>   
-        <Navbar className="bg-slate-800 sticky top-0 z-50">
+        <Navbar className="sticky top-0 z-50">
          <Link to="/" className="self-center text-nowrap text-sm md:text-lg dark:text-white">
          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500
           to-pink-500 rounded-lg text-white
@@ -21,19 +23,21 @@ function Headers() {
            <TextInput
            type="text"
            placeholder="search...."
-            rightIcon={FaSearch}
+            rightIcon={AiOutlineSearch}
             className="hidden md:inline"
            />
            </form>
            {/*  button for smallers  screen*/}
            <button className=" pl-4 w-12 h-9  md:hidden bg-gray-700 rounded-lg">
-             <FaSearch/>
+             <AiOutlineSearch/>
            </button>
          
            {/*  for dark and light screen  we create button and for signIn*/}
            <div className=" flex gap-2 md:order-2">
-               <Button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-200 font-small rounded-xl  h-11 text-sm px-3 py-2 me-2 mb-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-               <MdDarkMode/>
+               <Button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-200 font-small rounded-xl  h-11 text-sm px-3 py-2 me-2 mb-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                onClick={()=>dispatch(toggleTheme())}
+               >
+                {theme === 'light' ? <FaSun /> : <FaMoon />}
                </Button>
             
             {/* we have to signIn button */}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,15 @@ import {store} from './redux/store/store.js'
 import {Provider} from 'react-redux'
 import './index.css'
 import App from './App.jsx'
+import ThemeProvider from './components/ThemeProviders.jsx'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <Provider store={store}>
-     <App/>
+   <ThemeProvider >
+       <App/>
+     </ThemeProvider>
+      
    </Provider>
 )
